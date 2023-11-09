2ec1c5cc76a8149323032a242c3fd218acc52148
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2023-11-09 10:00:00 +0530

    add: fetch map provider by ID

--- src/controllers/map.ts
+++ src/controllers/map.ts
@@ -17,3 +17,28 @@ export const list_all = async (req: Request, res: Response) => {
         });
     }
 };
+
+export const list_id = async (req: Request, res: Response) => {
+    try {
+        const id = req.params.id;
+        const map = Maps.find((map) => map.id === id);
+
+        if (!map) {
+            return res.status(404).json(<BasicResponse>{
+                method: "MAP",
+                status: res.statusCode,
+                message: `Map with ID : '${id}' not found.`,
+            });
+        }
+
+        return res.status(200).json(map);
+    } catch (error) {
+        process.env.NODE_ENV !== "production" && console.error(error);
+
+        return res.status(500).json(<BasicResponse>{
+            method: "MAP",
+            status: res.statusCode,
+            message: `Internal Server Error : ${req.originalUrl}:list_id()`,
+        });
+    }
+};
--- src/routes/map.ts
+++ src/routes/map.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { list_all } from "../controllers/map.js";
+import { list_all, list_id } from "../controllers/map.js";
 
 const router = Router();
 
 router.get("/", list_all);
+router.get("/:id", list_id);
 
 export default router;
