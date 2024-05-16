58de7a1cabb970a91821b3e9a438d7c20ec99607
Pritam Halder <61552390+poseidon-code@users.noreply.github.com>
2024-05-16 10:00:00 +0530

    add: concurrent tile downloads

--- src/controllers/database.ts
+++ src/controllers/database.ts
@@ -2,9 +2,11 @@ import path from "node:path";
 import fs from "node:fs";
 
 import { Request, Response } from "express";
+import plimit from "p-limit";
 
 import type { BasicResponse } from "../models/BasicResponse.js";
-import Maps from "../data/maps.json";
+
+import Maps from "../data/maps.json" assert { type: "json" };
 
 type Coordinates = {
     latitude: number;
@@ -19,28 +21,35 @@ type Tile = {
     z: ZoomLevel;
 };
 
-/**
- *  {
- *      "top_left_coordinates" : {
- *          "latitude" : <value>,
- *          "longitude" : <value>
- *      },
- *      "bottom_right_coordinates" : {
- *          "latitude" : <value>,
- *          "longitude" : <value>
- *      },
- *      "zoom_levels" : {
- *          "from" : <value>,
- *          "to" : <value>
- *      } | {
- *          "at" : <value>
- *      }
- *  }
- */
+type TileFetch = { url: string; path: string };
+
 type RequestBody = {
+    /**
+     *  {
+     *      "top_left_coordinates" : {
+     *          "latitude" : <value>,
+     *          "longitude" : <value>
+     *      },
+     *      "bottom_right_coordinates" : {
+     *          "latitude" : <value>,
+     *          "longitude" : <value>
+     *      },
+     *      "zoom_levels" : {
+     *          "from" : <value>,
+     *          "to" : <value>
+     *      } | {
+     *          "at" : <value>
+     *      }
+     *  }
+     */
     top_left_coordinates: Coordinates;
     bottom_right_coordinates: Coordinates;
     zoom_levels: { from: ZoomLevel; to: ZoomLevel } | { at: ZoomLevel };
+};
+
+type RequestParams = {
+    /* `.../database/local/<id>` */
+    id: string;
 };
 
 /**
@@ -65,7 +74,7 @@ const tile_id = (coordinate: Coordinates, zoom: ZoomLevel): Tile => {
  * Formats the tile provider URL with tile coordinates. \
  * Replaces `{x}`, `{y}` & `{z}` in the URL with given tile coordinates.
  */
-const fetch_url = (url: string, tile: Tile): string => {
+const tile_fetch_url = (url: string, tile: Tile): string => {
     return url
         .replace("{x}", tile.x.toString())
         .replace("{y}", tile.y.toString())
@@ -73,29 +82,42 @@ const fetch_url = (url: string, tile: Tile): string => {
 };
 
 /**
- * `POST` : `/database/:id` \
+ * Fetch tile image from the `tile.url` and save it into `tile.path` location \
+ * Returns empty promises on rejection & resolution
+ */
+const tile_fetch = async (tile: TileFetch): Promise<any> => {
+    try {
+        const res: globalThis.Response = await fetch(tile.url);
+
+        if (res.ok) {
+            // save the successfully fetched image to server's storage
+            const data: ArrayBuffer = await res.arrayBuffer();
+            const buffered_data: Buffer = Buffer.from(data);
+            fs.writeFile(tile.path, buffered_data, (error) => {
+                if (error) return Promise.reject("failed to save the tile image");
+            });
+        }
+    } catch (error) {
+        return Promise.reject("failed to fetch tile image from provider");
+    }
+
+    return Promise.resolve("tile image fetched successfully");
+};
+
+/**
+ * Limits the maximum number of concurrent threads on `Promise.all()`
+ */
+const concurrency_limit = plimit(300);
+
+/**
+ * `POST` : `/database/local/:id` \
  * Downloads tiles for the requestd map `id` to the server's storage. \
  * Tiles are download for a regian bounded by the top-left corner coordinates & bottom-right corner coordinates \
  * of the bounded regian, either for a given zoom level or for a range of zoom levels.
  */
-export const download_tiles_local = async (req: Request<{ id: string }, {}, RequestBody>, res: Response) => {
+export const download_tiles_local = async (req: Request<RequestParams, {}, RequestBody>, res: Response) => {
     try {
         const id = req.params.id; // retrieve the map `id` from request parameters
-
-        const tiles_directory = process.env.TILES_DIRECTORY; // retrieve the map tiles directory of the server (environment variable)
-
-        if (!tiles_directory) {
-            // map tiles directory doesn't exits (i.e. `TILES_DIRECTORY` environment variable not set)
-            return res
-                .status(404)
-                .json(
-                    <BasicResponse>{
-                        method: "DATABASE",
-                        status: res.statusCode,
-                        message: `Path to tiles directory not found, set 'TILES_DIRECTORY' environment variable with path to map tiles directory.`,
-                    }
-                ); // prettier-ignore
-        }
 
         const map = Maps.find((map) => map.id === id); // retrieve the map properties using map `id`
 
@@ -112,12 +134,27 @@ export const download_tiles_local = async (req: Request<{ id: string }, {}, Requ
                 ); // prettier-ignore
         }
 
+        const tiles_directory = process.env.TILES_DIRECTORY; // retrieve the map tiles directory of the server (environment variable)
+
+        if (!tiles_directory) {
+            // map tiles directory doesn't exits (i.e. `TILES_DIRECTORY` environment variable not set)
+            return res
+                .status(404)
+                .json(
+                    <BasicResponse>{
+                        method: "DATABASE",
+                        status: res.statusCode,
+                        message: `Path to tiles directory not found, set 'TILES_DIRECTORY' environment variable with path to map tiles directory.`,
+                    }
+                ); // prettier-ignore
+        }
+
         // retrieve the bounded region coordinates & zoom levels
         const { top_left_coordinates, bottom_right_coordinates, zoom_levels }: RequestBody = req.body;
 
         // determine the starting & ending zoom levels
-        let start_zoom = 1;
-        let end_zoom = 1;
+        let start_zoom: number = 1;
+        let end_zoom: number = 1;
 
         if ("at" in zoom_levels) {
             // `at` property exists, then starting & endig zoom levels are same
@@ -142,73 +179,42 @@ export const download_tiles_local = async (req: Request<{ id: string }, {}, Requ
                 ); // prettier-ignore
         }
 
-        let fetch_tile_promises = []; // for holding fetch requests of all tile images
+        let tile_urls: TileFetch[] = []; // for holding fetch request URLs of all tile images
 
-        // if range of zoom levels is passed (i.e. `from` & `to` properties present)
+        // loop over zoom levels and download all tile images
         for (let z = start_zoom; z <= end_zoom; z++) {
-            const start_tile: Tile = tile_id(top_left_coordinates, z); // get the positions of the starting tile at this zoom level
-            const end_tile: Tile = tile_id(bottom_right_coordinates, z); // get the positions of the starting tile at this zoom level
+            const start_tile: Tile = tile_id(top_left_coordinates, z); // get the position of the starting tile at this zoom level
+            const end_tile: Tile = tile_id(bottom_right_coordinates, z); // get the position of the ending tile at this zoom level
 
             for (let y = start_tile.y; y <= end_tile.y; y++) {
                 for (let x = start_tile.x; x <= end_tile.x; x++) {
                     // create the complete tiles directory path as per the tile coordinates, i.e.
                     // `/<TILES_DIRECTORY>/<MAP_ID>/<ZOOM_LEVEL>/<Y>/`
-                    const tile_directory = path.join(tiles_directory, map.id, z.toString(), y.toString());
+                    const tile_directory: string = path.join(tiles_directory, map.id, z.toString(), y.toString());
 
                     // create the complete tile path at current position of `x` & `y` values
                     // `/<TILES_DIRECTORY>/<MAP_ID>/<ZOOM_LEVEL>/<Y>/<ZOOM_LEVEL>_<Y>_<X>.<FILE_EXTENSION>
-                    const tile_path = path.join(tile_directory, `${z}_${y}_${x}.${map.extension}`);
+                    const tile_path: string = path.join(tile_directory, `${z}_${y}_${x}.${map.extension}`);
 
                     if (!fs.existsSync(tile_path)) {
-                        // if the tile image doesn't exists, create the directory of that tile
-                        if (!fs.existsSync(tile_directory)) fs.mkdirSync(tile_directory, { recursive: true });
+                        // if the tile doesn't exists, then download the tile
+                        if (!fs.existsSync(tile_directory)) {
+                            // if the tile image's directory doesn't exists, create the directory of that tile
+                            fs.mkdirSync(tile_directory, { recursive: true });
+                        }
 
                         // format the tile provider URL for the map with the tile's `x`, `y`, & `z` positions
-                        const tile_url = fetch_url(map.provider_url, { x, y, z });
+                        const tile_url: string = tile_fetch_url(map.provider_url, { x, y, z });
 
-                        // add a fetch promise for the tile from the provider
-                        fetch_tile_promises.push(
-                            await fetch(tile_url).then(async (fetch_response) => {
-                                if (fetch_response.ok) {
-                                    // save the successfully fetched image to server's storage
-                                    const data = await fetch_response.arrayBuffer();
-                                    const buffered_data = Buffer.from(data);
-
-                                    fs.writeFile(tile_path, buffered_data, (write_error) => {
-                                        if (write_error) {
-                                            // failed to save the tile image
-                                            return res
-                                                .status(400)
-                                                .json(
-                                                    <BasicResponse>{
-                                                        method: "DATABASE",
-                                                        status: res.statusCode,
-                                                        message: `Error saving the tile image (${z}_${y}_${x}.${map.extension})`,
-                                                    }
-                                                ); // prettier-ignore
-                                        }
-                                    });
-                                } else {
-                                    // failed to fetch the tile image
-                                    return res
-                                        .status(400)
-                                        .json(
-                                            <BasicResponse>{
-                                                method: "DATABASE",
-                                                status: res.statusCode,
-                                                message: `Error downloading the tile image (${z}_${y}_${x}.${map.extension})`,
-                                            }
-                                        ); // prettier-ignore
-                                }
-                            })
-                        );
+                        // push the formatted tile URL and its image's saving path for downloading concurrently
+                        tile_urls.push({ url: tile_url, path: tile_path });
                     }
                 }
             }
         }
 
         // fetch all the tiles concurrently
-        await Promise.all(fetch_tile_promises);
+        await Promise.allSettled(tile_urls.map((tile) => concurrency_limit(() => tile_fetch(tile))));
 
         // all tile images have downloaded successfully
         return res
@@ -217,7 +223,7 @@ export const download_tiles_local = async (req: Request<{ id: string }, {}, Requ
                 <BasicResponse>{
                     method: "DATABASE",
                     status: res.statusCode,
-                    message: `All tiles were added`,
+                    message: `All tiles were added to server`,
                 }
             ); // prettier-ignore
     } catch (error) {
